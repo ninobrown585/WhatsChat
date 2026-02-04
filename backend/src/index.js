56361064f122133bdeb3662c8db17bdb3d3b2de4
Adrian Brown <ninobrown58518@gmail.com>
2026-02-03 20:43:36 -0600

    retry

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,11 +27,13 @@ app.use(cookieParser());
 app.use("/api/auth", auth);
 app.use("/api/message", messageRoutes);
 
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("(.*)", (req, res) => {
-    res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"));
-  });
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  }); 
 }
 
 server.listen(PORT, () => {
